38c17582481def788b7f5f339965b3052d503ab3
samuelt1822 <samuel.te@husky.neu.edu>
2019-04-13 02:26:06 -0700

    Testing Heroku setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Testing Heroku is set up correctly.
+
 const routes: Routes = [];
 
 @NgModule({
